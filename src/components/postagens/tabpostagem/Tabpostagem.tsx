12ffd0fa3de496d581ef0529dd332e1ea4add19d
IsmaelMF9469 <112709586+IsmaelMF9469@users.noreply.github.com>
2022-12-16 10:07:50 -0300

    update:tabpostagem

--- src/components/postagens/tabpostagem/Tabpostagem.tsx
+++ src/components/postagens/tabpostagem/Tabpostagem.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import { AppBar, Tab, Tabs, Typography } from '@material-ui/core';
 import {Box} from '@mui/material';
 import { TabContext, TabPanel } from '@material-ui/lab';
-import './TabPostagem.css';
+import './Tabpostagem.css';
 import ListaPostagem from '../listapostagem/Listapostagem';
 
 
